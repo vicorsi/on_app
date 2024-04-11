57b7a199d7ea4635575e1790efe3e87d54d3fd00
jzsantana <juliass7187@gmail.com>
2024-04-11 16:39:11 -0300

    modified user home screen

--- store/src/screens/Homepage/Homepage.tsx
+++ store/src/screens/Homepage/Homepage.tsx
@@ -59,7 +59,7 @@ const styles = StyleSheet.create({
         flex:1,
     },
     principalContainer:{
-        backgroundColor: '#f80',
+        backgroundColor: '#fff',
         width: '100%',
         height: '100%',
         display: 'flex',
