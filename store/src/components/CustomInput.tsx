8ff87020e65da215036b2460683006827755ee06
jzsantana <juliass7187@gmail.com>
2024-04-11 15:52:10 -0300

    modified props for custom input component

--- store/src/components/CustomInput.tsx
+++ store/src/components/CustomInput.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
-import { View, Text, TextInput, ViewProps } from "react-native";
+import { Text, TextInput, ViewProps } from "react-native";
 import styled from 'styled-components/native';
-
-interface CustomInputBaseProps{
-  width?: string; 
-}
-
-interface CustomInputProps extends CustomInputBaseProps {
-  label: string;
-  typeInput?: boolean;
-}
+import { CustomInputProps } from '../interfaces';
 
 export const Container = styled.View<ViewProps, TextInput>`
-  /* width: ${(props) => props.width ? props.width : 'auto'}; */
   width: 50%;
   height: 70px;
   display: flex;
@@ -32,11 +23,10 @@ export const InputBase = styled.TextInput`
 `;
 
 export const CustomInput: React.FC<CustomInputProps> = ({ label, typeInput, ...rest }) => {
-  // Passa explicitamente as propriedades necessárias
   return (
     <Container {...rest}>
       <Text>{label}</Text>
-      <InputBase secureTextEntry={typeInput} {...rest} /* Passa todas as propriedades aqui */ />
+      <InputBase secureTextEntry={typeInput} {...rest} />
     </Container>
   );
 };
