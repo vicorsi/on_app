10758499ff9a86ddfefad5e098faa6d7cc94fe9b
jzsantana <juliass7187@gmail.com>
2024-04-25 16:04:10 -0300

    update constants

--- app/constants.tsx
+++ app/constants.tsx
@@ -3,7 +3,10 @@ import { Dimensions } from "react-native";
 export const COLORS = {
     black: '#000',
     white: '#FFF',
-    lightGrey: '#F8F8F8',
+    lightGrey1: '#F8F8F8',
+    lightGrey2: '#F0F0F0',
+    lightGrey3: '#EBEBEB',
+    lightGrey4: '#C7C7C7',
     lightOrange: '#FB8500',
     principalOrange: '#FA5805',
 }
@@ -12,4 +15,3 @@ export const SIZE = {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
 };
-
