68cfaf3897a8955eaf9f12d827e0053ae93487e0
jzsantana <juliass7187@gmail.com>
2024-04-25 16:20:18 -0300

    feat: create header

---
+++ app/src/components/Header.tsx
@@ -0,0 +1,63 @@
+import { View, StyleSheet, Text, Pressable, StatusBar } from "react-native";
+import { COLORS } from "../../constants";
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import { useEffect, useState } from "react";
+import * as Font from 'expo-font';
+
+export default function Header() {
+    const [fontsLoaded, setFontsLoaded] = useState(false);
+    const loadFonts = async () => {
+        try {
+            await Font.loadAsync({
+                'Koulen': require('../assets/Fonts/Koulen/Koulen-Regular.ttf'),
+            });
+            setFontsLoaded(true);
+        } catch (error) {
+            console.error('Erro ao carregar fontes:', error);
+        }
+    };
+
+    useEffect(() => {
+        loadFonts();
+    }, []);
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
+    return (
+        <View style={styles.container}>
+            {/* <StatusBar backgroundColor={COLORS.principalOrange} animated={true} /> */}
+            <View style={styles.spacingContainer}>
+                <Text style={styles.logoText}>STYLE ON</Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        backgroundColor: COLORS.black,
+        height: '7%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        top: 0
+    },
+    spacingContainer: {
+        width: '75%',
+        height: '90%',
+        backgroundColor: COLORS.black,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+
+    },
+    logoText:{
+        fontFamily: 'Koulen',
+        fontSize: 34,
+        color: COLORS.principalOrange,
+        letterSpacing: 2
+    },
+})
